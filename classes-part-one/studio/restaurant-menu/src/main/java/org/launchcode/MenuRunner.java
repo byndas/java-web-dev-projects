3712ada0e85d95337b45cb22b184809058d69b4c
Marc Byndas <byndasma@gmail.com>
2024-04-10 18:49:09 -0400

    chapter four studio part one restaurant sketch

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuRunner.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuRunner.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class MenuRunner {
 
     public static void main(String[] args) {
-        Date date = new Date();
-        Menu menu = new Menu("Jolly Dervish", date);
+        Date menuCreationDate = new Date();
+        Menu menu = new Menu("Jolly Dervish", menuCreationDate);
     }
 }
