1ce07fae06b544d0eda80317ca36f536c6a49298
Marc Byndas <byndasma@gmail.com>
2024-04-15 17:03:15 -0400

    rewrote java unit test studio commented instructions

--- unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
+++ unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
@@ -1,32 +1,14 @@
 package org.launchcode;
 public class BalancedBrackets {
-    /*
-     * The function BalancedBrackets should return true if and only if
-     * the input string has a set of "balanced" brackets.
-     *
-     * That is, whether it consists entirely of pairs of opening/closing
-     * brackets (in that order), none of which mis-nest. We consider a bracket
-     * to be square-brackets: [ or ].
-     *
-     * The string may contain non-bracket characters as well.
-     *
-     * These strings have balanced brackets:
-     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
-     *
-     * While these do not:
-     *   "[LaunchCode", "Launch]Code[", "[", "]["
-     *
-     * @param str - to be validated
-     * @return true if balanced, false otherwise
-     */
+//  BalancedBrackets returns true if input string contains "[]" sets
+//      returns true:    "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+//      returns false:   "[LaunchCode", "Launch]Code[", "[", "]["
+
     public static boolean hasBalancedBrackets(String str) {
-        int brackets = 0;
+        int bracketsNum = 0;
         for (char ch : str.toCharArray()) {
-            if (ch == '[') {
-                brackets++;
-            } else if (ch == ']') {
-                brackets--;
-            }
+            if (ch == '[') { bracketsNum++; }
+            if (ch == ']') { bracketsNum--; }
         }
         return brackets == 0;
     }
--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -1,13 +1,34 @@
 package org.launchcode;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+//  write 12 unit tests to find errors in BalancedBrackets,
+//      to guide source code revisions_
+//  use TDD for expected BalancedBrackets class behavior_
+//  how should the class behave?
+//  correct the class to pass failed tests_
+//  write 12 input examples with their expected boolean return value_
 
 class BalancedBracketsTest {
-    //TODO: add tests here
-    @Test
-    public void emptyTest() {
+    @Test public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+/*
+    @Test public void () {}
+    @Test public void () {}
+    @Test public void () {}
+    @Test public void () {}
+    @Test public void () {}
+    @Test public void () {}
+    @Test public void () {}
+    @Test public void () {}
+    @Test public void () {}
+    @Test public void () {}
+    @Test public void () {}
+    @Test public void () {}
+ */
 }
