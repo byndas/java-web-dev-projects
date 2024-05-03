2167820b415c5d3a3362238184846b6c4ecdffdd
Marc Byndas <byndasma@gmail.com>
2024-05-03 11:47:33 -0400

    hellospring demo updated

---
+++ thymeleaf/exercise/codingevents/src/main/java/launchcode/org/CodingeventsApplication.java
@@ -0,0 +1,14 @@
+package launchcode.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingeventsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CodingeventsApplication.class, args);
+	}
+
+}
